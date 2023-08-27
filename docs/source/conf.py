ef2cdef4bc70f48d1a8cf8645ad940e7338bdc55
Fuad <fuad@koble.ai>
2023-08-27 17:11:00 +0200

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+import sys
+
 # -- Project information
 
 project = 'LexFlow'
@@ -28,10 +31,24 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# Add the project directory to the system path
+sys.path.insert(0, os.path.abspath('..'))
+
 # -- Options for HTML output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 # html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# -- Run sphinx-apidoc automatically
+
+def run_apidoc(_):
+    from sphinx.ext.apidoc import main
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    module_path = os.path.join(cur_dir, "..", "..", "app")  # adjust to point to the 'app' directory
+    main(['-e', '-f', '-o', os.path.join(cur_dir, 'generated'), module_path])  # output to the 'generated' directory
+
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
